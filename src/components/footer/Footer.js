0189d603b3702bd21ce1a7d3b4f55ed41c3598bf
Sascha Collinet <collinet.sascha@gmail.com>
2021-09-15 17:33:39 +0200

    add today's tasks

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,21 +3,47 @@ import Gryffindor from '../images/gryffindor.png'
 import Slytherin from '../images/slytherin.png'
 import Ravenclaw from '../images/ravenclaw.png'
 import Hufflepuff from '../images/hufflepuff.png'
+import { useState } from 'react'
 
 function Footer() {
+  const [activeHouse, setActiveHouse] = useState('Gryffindor')
   return (
     <footer className="footer">
       <nav className="footer__nav">
-        <button className="footer__button" id="footer__gryffindor">
+        <button
+          className={`footer__button ${
+            activeHouse === 'Gryffindor' ? 'footer-active' : ''
+          }`}
+          id="footer__gryffindor"
+          onClick={() => setActiveHouse('Gryffindor')}
+        >
           <img src={Gryffindor} alt="Gryffindor" className="footer__img" />
         </button>
-        <button className="footer__button" id="footer__slytherin">
+        <button
+          className={`footer__button ${
+            activeHouse === 'Slytherin' ? 'footer-active' : ''
+          }`}
+          id="footer__slytherin"
+          onClick={() => setActiveHouse('Slytherin')}
+        >
           <img src={Slytherin} alt="Slytherin" className="footer__img" />
         </button>
-        <button className="footer__button" id="footer__ravenclaw">
+        <button
+          className={`footer__button ${
+            activeHouse === 'Ravenclaw' ? 'footer-active' : ''
+          }`}
+          id="footer__ravenclaw"
+          onClick={() => setActiveHouse('Ravenclaw')}
+        >
           <img src={Ravenclaw} alt="Ravenclaw" className="footer__img" />
         </button>
-        <button className="footer__button" id="footer__hufflepuff">
+        <button
+          className={`footer__button ${
+            activeHouse === 'Hufflepuff' ? 'footer-active' : ''
+          }`}
+          id="footer__hufflepuff"
+          onClick={() => setActiveHouse('Hufflepuff')}
+        >
           <img src={Hufflepuff} alt="Hufflepuff" className="footer__img" />
         </button>
       </nav>
